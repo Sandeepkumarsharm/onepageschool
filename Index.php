798a2ce6181b80d5f15b669beed4ffc0f4700f29
Sandeepkumarsharm <154120195+Sandeepkumarsharm@users.noreply.github.com>
2023-12-18 12:01:52 +0530

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,269 @@
+<!doctype html>
+<html>
+    <head><title></title>
+<link rel="stylesheet" href="CSS/style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    </head>
+<body>
+<div class="width-100 top-header">
+<div class="container">
+<div class="width-100">
+<b class="news-list">NEWS:</b>
+<marquee class="headquote">It is My School</marquee>
+</div>
+</div>
+</div>
+
+<div class="width-100 header-menu">
+<div class="container">
+<div class="logo"><img  src="Images/logo berokala.jpg" width="150px" height="80px"></div>
+<ul class="main-menu">
+    <li><a href="#">Home</a></li>
+    <li><a href="#">About US</a></li>
+    <li><a href="#">Acadami</a></li>
+    <li><a href="#">Addmission</a></li>
+    <li><a href="#">Gallery</a></li>
+    <li><a href="#">NoticeBoard</a></li>
+    <li><a href="#">Contact US</a></li>
+</ul>
+</div>
+</div>
+
+<div class="width-100">
+<img class="slider-width" src="Images/schoolimg.jpg">
+</div>
+
+<div class="width-100 margin-top-50">
+<div class="container">
+    <div class="width-33">
+        <div class="latest-news">
+            <div class="heading-sect">
+                <h3 class="head-title">latest news</h3>
+            </div>
+            
+            <marquee width="60%" direction="up" height="280px">
+               <ul class="latest-news-ul">
+                 <li>Admmission is Going on <img src="Images/latesnewsblink.gif" width="30px" height="30px"> </li>
+                 <li>Admmission is Going on to <img src="Images/latesnewsblink.gif" width="30px" height="30px"></li>
+                 <li>Admmission is Going on to complete your <img src="Images/latesnewsblink.gif" width="30px" height="30px"></li>
+                 <li>Admmission is Going on to complete your <img src="Images/latesnewsblink.gif" width="30px" height="30px"></li>
+                 <li>Admmission is Going on to complete <img src="Images/latesnewsblink.gif" width="30px" height="30px"></li>
+                 <li>Admmission is Going on to complete <img src="Images/latesnewsblink.gif" width="30px" height="30px"></li>
+                 <li>Admmission is Going on to complete <img src="Images/latesnewsblink.gif" width="30px" height="30px"></li>
+
+               </ul>
+            </marquee>
+        
+        </div>
+    </div>
+
+
+    
+    <div class="width-33">
+        <div class="event-list">
+            <div class="heading-sect">
+                <h3 class="head-title">Upcoming Events</h3>
+            </div>
+<ul class="upcoming-event-list">
+<li><span class="event-date">29<br>September</span><span>Seminar is conducted On Inter(12th)</span></li>
+<li><span class="event-date">30<br>September</span><span>Seminar is conducted On Matriculation(10th)</span></li>
+<li><span class="event-date">1<br>October</span><span>Seminar is conducted On (8th)</span></li>
+<li><span class="event-date">2<br>October</span><span>Seminar is conducted On (8th)</span></li>
+<li><span class="event-date">3<br>October</span><span>Seminar is conducted On (7th)</span></li>
+<li><span class="event-date">4<br>October</span><span>Seminar is conducted On (6th)</span></li>
+<li><span class="event-date">35<br>October</span><span>Seminar is conducted (5th)</span></li>
+</ul>
+</div>
+</div>
+
+<div class="width-33">
+    <div class="notice-board">
+        <div class="heading-sect">
+            <h3 class="head-title">Notice Board</h3>
+        </div>
+<ul class="notice-board-list">
+    <li><img src="Images/fileicon.png" width="30px" height="30px"> in Climate Correction Day Challinge for</li>
+    <li><img src="Images/fileicon.png" width="30px" height="30px"> in Climate Correction Day Challinge for mission Life</li>
+    <li><img src="Images/fileicon.png" width="30px" height="30px"> in Climate Correction Day Challinge for mission Life</li>
+    <li><img src="Images/fileicon.png" width="30px" height="30px"> in Climate Correction Day Challinge for mission Life</li>
+    <li><img src="Images/fileicon.png" width="30px" height="30px"> in Climate Correction Day Challinge for mission Life</li>
+    <!--<li><img src="Images/fileicon.png" width="30px" height="30px"> in Climate Correction Day Challinge for mission Life</li>
+    <li><img src="Images/fileicon.png" width="30px" height="30px"> in Climate Correction Day Challinge for mission Life</li>
+    <li><img src="Images/fileicon.png" width="30px" height="30px"> in Climate Correction Day Challinge for mission Life</li>-->
+</ul>
+</div>
+</div>
+
+</div>
+</div>
+
+<div class="width-100">
+    <div class="container">
+        <div class="width-33">
+            <div class="our-Principal">
+                <div class="heading-sect">
+                    <h3 class="head-title">Useful Information</h3>
+                </div>
+                
+                   <ul class="useful-info-ul">
+                   <li><img class="sicone" src="Images/onlinesrvcicon.png" width="30px" height="30px"><a href="#">Online Service</a></li>
+                   <li><img class="sicone" src="Images/onlinereslticon.png"width="30px" height="30px"><a href="#">Online Result</a></li>
+                   <li><img class="sicone" src="Images/timeblicone.png"    width="30px" height="30px"><a href="#">Exam Time Table</a></li>
+                   <li><img class="sicone" src="Images/tendersicone.png"   width="30px" height="30px"><a href="#">Tenders</a></li>
+                   <li><img class="sicone" src="Images/gradeicone.png"     width="30px" height="30px"><a href="#">Check Your Grade </a></li>
+                   
+                </ul>
+
+
+  </div>
+  </div>
+
+
+  <!--srart about principal-->
+  <div class="width-33">
+    <div class="about-us">
+        <div class="heading-sect">
+            <h3 class="head-title">Useful Information</h3>
+        </div>
+          <div class="width-100 govt-sect-style">
+
+            <div class="width-40">
+                <img class="govt-img" src="Images/manlaptop.jpg">
+                </div>
+                <div class="width-60">
+                    <p class="manname">Sandyaama</p>
+                    <p class="designation">Principal Of My Collage</p>
+                </div>
+
+            
+          </div>
+          <div class="width-40">
+            <img class="govt-img" src="Images/manlaptop.jpg">
+            </div>
+            <div class="width-60">
+                <p class="manname">Sandyaama</p>
+                <p class="designation">Principal Of My Collage</p>
+            </div>
+          </div>
+          </div>
+
+          <!-- For Certificate-->
+          <div class="width-33">
+            <div class="certificate">
+                <div class="heading-sect">
+                    <h3 class="head-title">Certificate--</h3>
+                </div>
+                <img src="Images/certifict.jpg" class="width-100">
+          </div>
+          </div>
+        
+</div>
+</div>
+
+<!--Start Gallery-->
+
+<div class="width-100 margin-top-50">
+    <div class="container">
+        <h3 class="gallery-head-style">Gallery</h3>
+        <div class="width-25"><img src="Images/glryimg0.jpg" class="gallery-img"></div>
+        <div class="width-25"><img src="Images/glryimg1.jpg" class="gallery-img"></div>
+        <div class="width-25"><img src="Images/glryimg2.jpg" class="gallery-img"></div>
+        <div class="width-25"><img src="Images/glryimg3.jpg" class="gallery-img"></div>
+        <div class="width-25"><img src="Images/glryimg4.jpg" class="gallery-img"></div>
+        <div class="width-25"><img src="Images/glryimg5.jpg" width="240px" height="250px" ></div>
+       <div class="width-25"><img src="Images/glryimg6.jpg" class="gallery-img"></div>
+        <div class="width-25"><img src="Images/glryimg7.jpg" width="290px" height="250px"></div>  
+                   
+    </div>
+    
+</div>
+<!--Recruiter-->
+<div class="width-100 margin-top-50">
+    <div class="container">
+        <h3 class="recruiter-head-style">Top Recruiter Company</h3>
+        <marquee class="recruiter-marquee">
+        <img src="Images/recruiter/brndimg1.jpg">
+        <img src="Images/recruiter/brndimg2.jpg">
+        <img src="Images/recruiter/brndimge3.jpg">
+        <img src="Images/recruiter/brndimg4.jpg">
+        <img src="Images/recruiter/brndimg5.jpg">
+        <img src="Images/recruiter/brndimg6.jpg">
+        <img src="Images/recruiter/brndimg7.jpg">
+        <img src="Images/recruiter/brndimg8.jpg">
+        <img src="Images/recruiter/brndimg9.jpg">
+        <img src="Images/recruiter/brndimg10.jpg">
+        </marquee>
+</div>
+</div>
+
+<!--Footer-->
+<div class="width-100 margin-top-50 footer">
+    <div class="container">
+        <div class="width-25">
+<h2 class="quicklink-heading">Quick Links</h2>
+<ul class="quicklink-menu">
+<li><a href="#">Home</a></li>
+<li><a href="#">About us</a></li>
+<li><a href="#">Gallery</a></li>
+<li><a href="#">Contact us</a></li>
+<li><a href="#">Social media</a></li>
+    
+</ul>
+</div>
+
+    <div class="width-25">
+        <h2 class="quicklink-heading">Student Section</h2>
+        <ul class="quicklink-menu">
+        <li><a href="#">Result</a></li>
+        <li><a href="#">Pay fee</a></li>
+        <li><a href="#">Training</a></li>
+        <li><a href="#">Placement</a></li>
+        
+            
+        </ul>
+        </div>
+        
+            </div>
+            <div class="width-25">
+                <h2 class="quicklink-heading">Information Links</h2>
+                <ul class="quicklink-menu">
+                <li><a href="#">News</a></li>
+                <li><a href="#">R&D Policy</a></li>
+                <li><a href="#">Anti Ragging</a></li>
+                <li><a href="#">Others</a></li>
+                
+                    
+                </ul>
+                </div>
+
+                <div class="width-25">
+                    <h2 class="quicklink-heading">Get In Touch</h2>
+                    <ul class="get-in-touch">
+                    <li><i class="fa fa-envelope-o"></i> :<a href="#" class="footer-e-mail">sandeep@gmail.com</a></li>
+                    <li><i class=" fa-headphones"></i>Whats-App: +9625656152</li>
+                    <li><i class="fa fa-fax"></i> NO: +75971030597</li>
+                    <li><i class="fa fa-globe"></i><a href="#" class="footer-website">https/www.goggle.com</a></li>
+         
+                    </ul>
+
+                    <ul class="social-media">
+                        <li><a href="#"><img src="Images/facebookicone.jpg" width="20px" height="20px"></a></li>
+                        <li><a href="#"><img src="Images/twitericone.jpg" width="20px" height="20px"></a></li>
+                        <li><a href="#"><img src="Images/telegram.jpg" width="20px" height="20px"></a></li>
+                        <li><a href="#"><img src="Images/linkdinicon.jpg" width="20px" height="20px"></a></li>
+                        <li><a href="#"><img src="Images/instaicone.jpg" width="20px" height="20px"></a></li>
+
+
+                    </ul>
+                    </div>
+</div>
+</div>
+
+<div class="width-100 footer2-bacbor">
+<p class="footer2-content">Copyright @ 2023, Sandy.com all right reserved</p>
+</div>
+
+
+</body>
+</html>
